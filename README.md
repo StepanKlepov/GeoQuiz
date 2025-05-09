5d52597b6ae3dc6329dd1d381ce1650d581d795f
Stepan Klepov <stepan.r.klepov@gmail.com>
2025-05-09 23:23:05 +0500

    feat: create Geo Quiz app
    The implementation is based on MathQuiz with the terms in the table
    replaced to the Geographical ones. The favicon and the logo are also
    updated accordingly.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GeoQuiz
+
 A game that can help you study Physical Geography terms in English
+
+https://stepanklepov.github.io/GeoQuiz
---
+++ index.js
@@ -0,0 +1,180 @@
+// index.js
+
+function randomInt(low, high) {
+  return Math.trunc(low + Math.random() * (high - low));
+}
+
+// https://learn.javascript.ru/task/shuffle
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; --i) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+}
+
+function deal(count, total) {
+  const a = Array(total)
+    .fill(0)
+    .map((x, i) => i);
+
+  return shuffle(a).slice(0, count);
+}
+
+function generateAnswerOptions(count, total, ixCorrect) {
+  let a = [ixCorrect];
+
+  while (a.length < count) {
+    const ix = randomInt(0, total);
+    if (!a.includes(ix)) {
+      a.push(ix);
+    }
+  }
+
+  return shuffle(a);
+}
+
+function reset(allTerms, numQuestions, numOptions) {
+  const questions = deal(numQuestions, allTerms.length);
+  const currentQuestion = 0;
+  const answerOptions = generateAnswerOptions(
+    numOptions,
+    allTerms.length,
+    questions[currentQuestion]
+  );
+
+  return [questions, currentQuestion, answerOptions];
+}
+
+function fillQuestion(allTerms, ixTerm, ixsOptions) {
+  const contQuestion = document.querySelector(".question");
+  const textTerm = contQuestion.querySelector(".term");
+  textTerm.innerText = allTerms[ixTerm][LANG_FROM];
+
+  const contOptions = contQuestion.querySelectorAll(".answer-option");
+  for (let i = 0; i < contOptions.length; i++) {
+    const textAnswer = contOptions[i].querySelector(".option-text");
+    textAnswer.innerText = allTerms[ixsOptions[i]][LANG_TO];
+  }
+}
+
+function fillCounter(idxCurrent, cntTotal) {
+  spanCntCurrent.innerText = idxCurrent + 1;
+  spanCntTotal.innerText = cntTotal;
+}
+
+function showResult(cntCorrect, cntTotal) {
+  spanStatsCorrect.innerText = cntCorrect;
+  spanStatsTotal.innerText = cntTotal;
+  spanStatsPct.innerText = ((cntCorrect / cntTotal) * 100).toFixed(0);
+  dlgResult.showModal();
+}
+
+async function loadJson(path) {
+  const response = await fetch(path);
+  const json = await response.json();
+  return json;
+}
+
+/******************************************************************************/
+
+const NUM_QUESTIONS = 20;
+const NUM_OPTIONS = 4;
+const LANG_FROM = "en";
+const LANG_TO = "ru";
+
+const formOptions = document.querySelector("#form-options");
+const buttonSubmit = document.querySelector("#button-submit");
+const buttonNext = document.querySelector("#button-next");
+const spanCntCurrent = document.querySelector("#span-counter-current");
+const spanCntTotal = document.querySelector("#span-counter-total");
+
+const dlgResult = document.querySelector("#dialog-result");
+const spanStatsCorrect = document.querySelector("#span-stats-correct");
+const spanStatsTotal = document.querySelector("#span-stats-total");
+const spanStatsPct = document.querySelector("#span-stats-pct");
+const buttonRestart = document.querySelector("#button-restart");
+
+let geo_terms = null;
+let [questions, currentQuestion, answerOptions] = [null, null, null];
+let cntCorrect = 0;
+
+function startQuiz() {
+  [questions, currentQuestion, answerOptions] = reset(
+    geo_terms,
+    NUM_QUESTIONS,
+    NUM_OPTIONS
+  );
+
+  cntCorrect = 0;
+  fillQuestion(geo_terms, questions[currentQuestion], answerOptions);
+  fillCounter(currentQuestion, NUM_QUESTIONS);
+}
+
+function resetDisplay() {
+  for (const option of formOptions.querySelectorAll(".answer-option")) {
+    option.classList.remove("answer-correct", "answer-wrong");
+  }
+
+  const radioFirst = formOptions.querySelector("input");
+  radioFirst.checked = true;
+
+  buttonSubmit.disabled = false;
+  buttonNext.disabled = true;
+}
+
+loadJson("./geo_terms.json").then((json) => {
+  geo_terms = json;
+  startQuiz();
+});
+
+formOptions.addEventListener("submit", (event) => {
+  const contOptions = formOptions.querySelectorAll(".answer-option");
+
+  for (let i = 0; i < contOptions.length; i++) {
+    const option = contOptions[i];
+    const radio = option.querySelector("input");
+    const isAnswerCorrect = answerOptions[i] === questions[currentQuestion];
+
+    if (radio.checked) {
+      if (isAnswerCorrect) {
+        ++cntCorrect;
+      } else {
+        option.classList.add("answer-wrong");
+      }
+    }
+
+    if (isAnswerCorrect) {
+      option.classList.add("answer-correct");
+    }
+  }
+
+  buttonNext.disabled = false;
+  buttonSubmit.disabled = true;
+
+  event.preventDefault();
+});
+
+buttonNext.addEventListener("click", (event) => {
+  console.log(currentQuestion + 1);
+
+  if (++currentQuestion >= NUM_QUESTIONS) {
+    showResult(cntCorrect, NUM_QUESTIONS);
+  } else {
+    answerOptions = generateAnswerOptions(
+      NUM_OPTIONS,
+      geo_terms.length,
+      questions[currentQuestion]
+    );
+    resetDisplay();
+    fillQuestion(geo_terms, questions[currentQuestion], answerOptions);
+    fillCounter(currentQuestion, NUM_QUESTIONS);
+  }
+});
+
+buttonRestart.addEventListener("click", (event) => {
+  dlgResult.close();
+  resetDisplay();
+  startQuiz();
+});
